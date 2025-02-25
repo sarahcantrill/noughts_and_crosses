4db660efaedb85007a6d31bdf2969c0204d054e8
sarahcantrill <25324420@edgehill.ac.uk>
2025-02-25 23:39:18 +0000

    adding some comments

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let currentPlayer = "X"
 let gameBoard = ["", "", "", "", "", "", "", "", ""]
 let gameActive = true
 
+//winning positions
 const winningPositions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -17,43 +18,53 @@ const winningPositions = [
     [2, 4, 6],
 ]
 
+//clicking event 
 function clickSquare(event) {
     let clickedSquare = event.target //or event.target
     // //const index = event.target.dataset.index;
     const clickedSquareIndex = Number.parseInt(clickedSquare.getAttribute("data-index"))
 
+    //check if the square has already been chosen
     if (gameBoard[clickedSquareIndex] !== "" || !gameActive){
         return; 
     }
 
+    //update square w players symbol
     gameBoard[clickedSquareIndex] = currentPlayer
     clickedSquare.textContent = currentPlayer
     clickedSquare.setAttribute("aria-label", `Cell ${clickedSquareIndex + 1}, ${currentPlayer}`);
 
     checkResult()
 
+    //switch players
     currentPlayer = currentPlayer === "X" ? "O" : "X";
 }
 
+// check if the game state is a win or a draw
 function checkResult() {
     for (let i = 0; i < winningPositions.length; i++) {
         const [a, b, c] = winningPositions[i];
 
         if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c] ) {
-            gameActive = false;
+            gameActive = false; //if there is a player in winning position then the game stops 
             document.getElementById("message").textContent = `${gameBoard[a]} wins`;
             return;
         }
     }
 
+    //once there is no squares left stop the game
     if(!gameBoard.includes("")) {
         gameActive = false;
         document.getElementById("message").textContent = "its a draw";
     }
 }
 
+//call the clicking of the squares
 document.querySelectorAll(".square").forEach(square => {
     square.addEventListener("click", clickSquare);
 })
 
+//
+
 });
+
