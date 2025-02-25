e93f65cb6587396370f310e9d65bc85b971e63c0
sarahcantrill <25324420@edgehill.ac.uk>
2025-02-25 15:18:47 +0000

    winning conditions

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ let currentPlayer = "X"
 let gameBoard = ["", "", "", "", "", "", "", "", ""]
 let gameActive = true
 
+const winningPositions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [6, 7, 8],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    
+]
+
 function clickSquare(event) {
     clickedSquare = clickedSquareEvent.target
 }
